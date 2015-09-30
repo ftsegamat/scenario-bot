b976693b82fb9d6108f2fd3474a89e7c83b9ca4a
ftsegamat <ftsegamat@gmail.com>
2015-09-30 10:20:56 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,3 +7,35 @@ import Validator from './scenario-wrapper.js';
 import Bot from './bot.js';
 
 export {Bot, Scenario, ScenarioWrapper, Validator, BotApiPromisified, StateHolder, JobQueue};
+
+//import 'babel/polyfill';
+import {Bot, Scenario} from 'telegram-scenario-bot';
+
+const scenarioApi = {
+  username() {
+    var from = this.from,
+        user_name = from.first_name || from.username;
+    return user_name;
+  },
+
+  doCalculate() {
+    var text = this.text,
+        session = this.session;
+
+    // ... some logic ...
+
+    // We may store to session. Session clear when enter to root scenario
+    session.acc = (session.acc || '') + text;
+  },
+
+  getCalcResult() {
+    return this.session.acc;
+  }
+};
+
+var script = /*... load yaml*/
+var bot_scenario = new Scenario(scenarioApi, script),
+    b = new Bot('124479481:AAEJSLQDxlmxowLsrAvNeUwlFtw3T2VOpck');
+
+b.scenario(bot_scenario);
+b.start();
